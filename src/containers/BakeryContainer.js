4f486672e03596dae08c0a6f3a36f047a196a7fe
Tadiwanashe Dzvoti <tadiwadzvoti@hotmail.com>
2024-03-21 15:56:51 +0000

    Started search thing

--- src/containers/BakeryContainer.js
+++ src/containers/BakeryContainer.js
@@ -8,29 +8,48 @@ const  BakeryContainer = () => {
 
     const [totalValueSold, setTotalValueSold] = useState(0);
 
+    const [filterTerm, setFilterTerm] = useState("");
+
     const sellItem = (price) => {
         setTotalValueSold(totalValueSold + price);
     }
 
     const [items, setItems] = useState(BakeryItemsPayload);
 
+    var itemsList = items.map((item) => <Item key={item.name} item={item} sellItem={sellItem}/>);
+
     const addNewItem = (newItem) => {
         setItems([...items, newItem]);
     }
 
-    const displayItems = (type) => {
-        return items.map((item) => <Item key={item.name} item={item} itemType={type} sellItem={sellItem}/>);
-    }
+    const filterItems = (() => {
+        let filterItemsList = items.filter(items => {items.name.toLowerCase().includes(filterTerm.toLowerCase)});
+        console.log(filterItemsList);
+        itemsList =  filterItemsList.map((item) => <Item key={item.name} item={item} sellItem={sellItem}/>);
+        console.log(itemsList);
+    });
 
     return (
         <>
+            <div className="search">
+                <form id="filter-items-form" onSubmit={filterItems}>
+                    <label htmlFor="filter-term">Search:</label>
+                    <input 
+                    id="filter-term" 
+                    type="text"
+                    onChange={(event)=> setFilterTerm(event.target.value)}
+                    />
+                    <input type="submit"/>
+                </form>
+            </div>
+
             <div className="add-item">
                 <AddItem addNewItem={addNewItem}/>
             </div>
             <div className="bakery-items">
                 <div className="cakes-section">
                     <h2>Cakes</h2>
-                    <div className="display">{displayItems("cakes")}</div>
+                    <div className="display">{itemsList}</div>
                 </div>
             </div>
             <h2>Total Value Sold: £{totalValueSold}</h2>
