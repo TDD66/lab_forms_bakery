eb5e60a6568a5e7b1dd58121afd99055cfb9e6aa
Tadiwanashe Dzvoti <tadiwadzvoti@hotmail.com>
2024-03-21 15:25:35 +0000

    Finished MVP

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -2,39 +2,53 @@ import { useState } from "react";
 
 const AddItem = ({addNewItem}) => {
 
-const [name, setName] = useState("");
-const [ingredients, setIngredients] = useState([]);
-const [price, setPrice] = useState(null);
-const [rating, setRating] = useState(null);
+    const [name, setName] = useState("");
+    const [ingredients, setIngredients] = useState([]);
+    const [price, setPrice] = useState(0);
+    const [rating, setRating] = useState(0);
 
-
-
-
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newItem = {name, ingredients, price, rating};
+        addNewItem(newItem);
+    }
 
 
 
     return ( 
     <>
-    <h2>Add Item</h2>
-    <form>
-        <input
-        type="text"
-        name="name"
-        placeholder="Enter item name"
-        value={name}
-        onChange={(event)=> setName(event.target.value)}
-        />
-         <input/>
-         <input/>
-         <input/>
-    </form>
-    
-
-
-
-    
-    
-    
+        <h2>Add Item</h2>
+        <form onSubmit={handleSubmit}>
+            <input
+            type="text"
+            name="name"
+            placeholder="Enter item name"
+            value={name}
+            onChange={(event)=> setName(event.target.value)}
+            />
+            <input
+                type="text"
+                name="ingredients"
+                placeholder="Enter ingredients"
+                value={ingredients}
+                onChange={(event)=> setIngredients(event.target.value.split(","))}
+            />
+            <input
+            type="number"
+            name="price"
+            placeholder="Enter item price"
+            value={price}
+            onChange={(event)=> setPrice(parseInt(event.target.value))}
+            />
+            <input
+            type="number"
+            name="rating"
+            placeholder="Enter item rating"
+            value={rating}
+            onChange={(event)=> setRating(parseInt(event.target.value))}
+            />
+            <input type="submit" value="submit"/>
+        </form>
     
     </> );
 
